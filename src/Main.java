dcc6d8d3134976f5470dac7ce569b3f0356b5ee2
Takuma Higa <hikatakushin@hikatakushinnoMacBook-Air.local>
2023-12-11 23:05:29 +0900

    step2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
 
     public static void main(String[] args){
-    String str = null;
-    System.out.println(str.length());
+        String str = null;
+        try{
+            System.out.println(str.length());
+        } catch (NullPointerException n){
+            System.out.println("NullPointerExceptionが発生しました" );
+            System.out.println(n.getMessage());
+        }
     }
 }
